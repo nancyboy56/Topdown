610f69d8845c7d0cff9a3ad6f23353bb9d534b38
Casey <gooby5656@gmail.com>
2026-04-24 16:07:04 +1000

    added timer

---
+++ TopDown/Assets/Scripts/Timer.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI timerText;
+    private float time = 0;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        timerText.text = Mathf.Floor(Time.time).ToString();
+    }
+}
