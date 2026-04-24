6daa51c9ffd6799db26f6ce48cef7aabc7cf9209
Casey <gooby5656@gmail.com>
2026-04-24 15:18:00 +1000

    collect object

---
+++ TopDown/Assets/Scripts/Collect.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Collect : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            
+            //might have an aimation before hand
+            //destory colletable
+            Destroy(gameObject);
+        }
+    }
+}
