5344b3809eab339a9712f99eff203f5df4e8a559
Casey <gooby5656@gmail.com>
2026-05-06 13:42:12 +1000

    fixed the south bound not working

--- TopDown/Assets/Scripts/WorldBounds.cs
+++ TopDown/Assets/Scripts/WorldBounds.cs
@@ -9,8 +9,7 @@ public class WorldBounds : MonoBehaviour
     [SerializeField]
     private bool flipY = false;
     
-    
-
+    // so it doesnt spawn into the trigger enter collider and loop forever
     [SerializeField] private Vector3 offset;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -24,6 +23,10 @@ public class WorldBounds : MonoBehaviour
         
     }
 
+    // only works if map is symmertical and square
+    // will have to work out when i have art bc i want it to look like a never ending sea
+    // not notice that you teleport
+    
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
